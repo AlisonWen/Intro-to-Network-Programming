c8ae4b2b0cab2ce0ffce90c4e4d0ef7feaf828f3
yunycu <100680150+yunycu@users.noreply.github.com>
2022-12-06 12:10:02 +0800

    Update server.cpp

--- Lab8/server.cpp
+++ Lab8/server.cpp
@@ -20,39 +20,75 @@
 #include <filesystem>
 #include <dirent.h>
 #include <vector>
-#include <unordered_map>
+#include <set>
+#include <map>
+#include <utility>
 
 #define err_quit(m) { perror(m); exit(-1); }
 using namespace std;
 
 struct Packet{
     int seq_idx;
+    int num_off;
+    int offset;
+    int checksum;
     string f_name;
-    string f_cnt;
 };
 
-unordered_map <int, int> record;
-int _check;
-// <seq num, <file name, content>>
-Packet decompose(string content){ // decompose a packet into 1.seq num, 2.checksum, 3.content(string)
+vector<int> packet_list;                    //record how many offset of a file should be received
+vector<bool> received_list;
+vector<set<int>> record_list;               //record whether the packet is received
+vector<vector<string>> fcnt_list;   //record the content of the packets
+
+
+bool checkIsExist(int f_idx, int p_idx){
+    if(record_list[f_idx].find(p_idx) == record_list[f_idx].end())
+        return 0;
+    else
+        return 1;
+}
+
+string MergePackets(int idx){
+    string cnt = "";
+    for(auto &frag : fcnt_list[idx]){
+        cnt += frag;
+    }
+    return cnt;
+}
+
+Packet decompose(string content){ // decompose a packet 
 	content.erase(content.begin()); // erase '<'
 	string fileName = "";
     int seq_num = 0;
+    int num_packet;
+    int off;
+    int _check;
 	stringstream ss;
     ss << content;
-    ss >> seq_num >> _check >> fileName;
+    ss >> seq_num >> num_packet >> off >> _check >> fileName;
 	auto start = content.find('>');
-	string _content = content.substr(start + 1, content.size() - start);
+	string fcnt = content.substr(start + 1, content.size() - start);
+    
+    //record how many offset 
+    packet_list[seq_num] = num_packet;
+    //if the content list hasn't been established
+    if(fcnt_list[seq_num].empty())
+        fcnt_list[seq_num].resize(num_packet);
+    //record the content to the list
+    if(!checkIsExist(seq_num, off))
+        fcnt_list[seq_num][off] = fcnt;
 
     Packet packet;
     packet.seq_idx = seq_num;
+    packet.num_off = num_packet;
+    packet.offset = off;
+    packet.checksum = _check;
     packet.f_name = fileName;
-    packet.f_cnt = _content;
 
 	return packet;
 }
 
-bool checksum(const char content[]){
+bool checksum(const char content[], int _check){
     vector <uint64_t> data;
 	uint64_t cur = 0;
 	char tmp[8];
@@ -82,10 +118,6 @@ int main(int argc, char *argv[]) {
 		return -fprintf(stderr, "usage: %s ... <port>\n", argv[0]);
 	}
 
-	setvbuf(stdin, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	setvbuf(stdout, NULL, _IONBF, 0);
-
 	memset(&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(strtol(argv[argc-1], NULL, 0));
@@ -99,47 +131,72 @@ int main(int argc, char *argv[]) {
     int file_num = atoi(argv[argc-2]);
     char* store_path = argv[argc-3];
 
+    //initialize the tables
+    fcnt_list.resize(file_num);
+    record_list.resize(file_num);
+    packet_list.resize(file_num);
+    received_list.resize(file_num);
+
     struct sockaddr_in csin;
     socklen_t csinlen = sizeof(csin);
     int rlen;
     char fileBuf[100000];
 
-    for(int i = 0; i < file_num;){
-        for(int num = 0; num < 10; num++){
-            bzero(fileBuf, sizeof(fileBuf));
-            if(rlen = recvfrom(s, fileBuf, 100000, MSG_DONTWAIT, (struct sockaddr*) &csin, &csinlen) <= 0){
-                //printf("Didn't receive the file text!\n");
-                char trash[] = "nono";
-                sendto(s, trash, strlen(trash), 0, (struct sockaddr *)&csin, csinlen); 
-                continue;
-            }
-            //pair<int, pair<string, string>> packet = decompose(string(fileBuf));
-            Packet packet = decompose(string(fileBuf));
-            if(record[packet.seq_idx]) {
-                string ack_msg = "<" + to_string(packet.seq_idx) + " 0>ACK";
-                continue;
-            }
+    int i = 0;
+    while(i < file_num){
+        bzero(fileBuf, sizeof(fileBuf));
+        if(rlen = recvfrom(s, fileBuf, 100000, 0, (struct sockaddr*) &csin, &csinlen) <= 0){
+            //printf("Didn't receive the file text!\n");
+            char trash[] = "nono";
+            sendto(s, trash, strlen(trash), 0, (struct sockaddr *)&csin, csinlen); 
+            continue;
+        }
+        //printf("received = %s\n", fileBuf);
+        Packet packet = decompose(string(fileBuf));
 
+        // if(checkIsExist(packet.seq_idx, packet.offset)){ // return ack and continue
+        //     string ack_msg = to_string(packet.seq_idx) + " " + to_string(packet.offset) + " ACK";
+        //     sendto(s, ack_msg.c_str(), ack_msg.length(), 0, (struct sockaddr *)&csin, csinlen); 
+        //     continue;
+        // }
+
+        string f_cnt = fcnt_list[packet.seq_idx][packet.offset];
+        if(!checksum(f_cnt.c_str(), packet.checksum)){   //checksum 不對
+            //printf("not right checksum!\n");
+            char trash[] = "nono";
+            sendto(s, trash, strlen(trash), 0, (struct sockaddr *)&csin, csinlen); 
+            continue;
+        }
+
+        //told client that the packet is received
+        string ack_msg = to_string(packet.seq_idx) + " " + to_string(packet.offset) + " ACK";
+        sendto(s, ack_msg.c_str(), ack_msg.length(), 0, (struct sockaddr *)&csin, csinlen); 
+        //cout<<"ack sent!!\n";
+
+        //deal with the received packets
+        if(!checkIsExist(packet.seq_idx, packet.offset))    //第一次收到packet
+            record_list[packet.seq_idx].insert(packet.offset);
+
+        if(record_list[packet.seq_idx].size() == packet_list[packet.seq_idx] && !received_list[packet.seq_idx]){  
+            //有任何一個file收到了所有的packet
+            //write file
+            i++;
+            received_list[packet.seq_idx] = true;
             FILE *fp;
-            int fsize = packet.f_cnt.size();
-            string _path = string(store_path) + "/" + packet.f_name;
-            fp = fopen(_path.c_str(), "w");
-            if(fp && checksum(packet.f_cnt.c_str())){
-                fwrite(packet.f_cnt.c_str(), fsize, 1, fp);
+            string cnt = MergePackets(packet.seq_idx);
+            string f_path = string(store_path) + "/" + packet.f_name;
+
+            fp = fopen(f_path.c_str(), "w");
+            if(fp){
+                fwrite(cnt.c_str(), cnt.size(), 1, fp);
                 printf("File received successfully.\n");
             }
             else{
                 printf("Cannot create to output file.\n");
             }
             fclose(fp);
-
-            //told client that the file is received
-            string ack_msg = "<" + to_string(packet.seq_idx) + " 0>ACK";
-            sendto(s, ack_msg.c_str(), ack_msg.length(), 0, (struct sockaddr *)&csin, csinlen); 
-            record[packet.seq_idx] = 1;
-            i++;
-            if(i >= file_num)break;
         }
     }
+
 	close(s);
 }
